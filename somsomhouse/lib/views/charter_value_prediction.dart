dd86684449db477c15380bd2c0b9d0f29acd0436
limeunbeen <mossi3627@naver.com>
2023-01-11 21:12:49 +0900

    [추가] 동별 예측화면 페이지

--- somsomhouse/lib/views/charter_value_prediction.dart
+++ somsomhouse/lib/views/charter_value_prediction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 
 class CharterPrediction extends StatefulWidget {
@@ -9,6 +10,11 @@ class CharterPrediction extends StatefulWidget {
 }
 
 class _CharterPredictionState extends State<CharterPrediction> {
+
+  // 계약 계절 드롭박스로 부르기 위해 리스트 작성
+  List <String> dropdownList = ['봄', '여름', '가을', '겨울'];
+  String? selectedDropdown = '';
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -65,22 +71,44 @@ class _CharterPredictionState extends State<CharterPrediction> {
                 ),
               ),
               const SizedBox(height: 20,),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: '계약 계절',
-                  labelText: '계약 계절',
-                  prefixIcon: const Icon(Icons.sunny_snowing),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+              Container(
+                width: 450,
+                height: 58,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Colors.black45, style: BorderStyle.solid, width: 0.8
                   ),
                 ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton2(
+                    // icon: Icon(Icons.park),
+                    hint: Text('계절'),
+                    isExpanded: true,
+                    value: selectedDropdown == '' ? null : selectedDropdown,
+                    items: dropdownList.map((String item) {
+                      return DropdownMenuItem<String>(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(30, 5, 0, 0),
+                          child: Text('$item'),
+                        ),
+                        value: item,
+                        );
+                        
+                    }).toList(), 
+                    onChanged: (dynamic value) {
+                      setState(() {
+                        selectedDropdown = value;
+                      });
+                    }),
+                ),
               ),
+              const SizedBox(height: 20,),
               Padding(
                 padding: const EdgeInsets.all(25.0),
                 child: ElevatedButton( 
-                  onPressed: (){
-                    //
-                  }, child: const Text('시세 예측해 보기')),
+                  onPressed: () => _showDialog(context),    
+                  child: const Text('시세 예측해 보기')),
               ),
             ],
           ),
@@ -90,5 +118,26 @@ class _CharterPredictionState extends State<CharterPrediction> {
       ),
     );
   }
+
+ // 입련된 내용을 바탕으로 예측화면 보여주기 위한 함수
+  // 만든 날짜 : 2022.01.11  
+  // 만든 사람 : 임은빈
+  _showDialog(BuildContext context) {
+    showDialog(
+    context: context, 
+    barrierDismissible: false,
+    builder: (BuildContext ctx) {
+      return AlertDialog(
+        title: const Text('예측'),
+        content: Text('전세값은 ?입니다.'),
+        actions: [
+          ElevatedButton(
+            onPressed: () => Navigator.of(ctx).pop(),
+            child: const Text('나가기'))
+        ],
+      );
+    });
+  }
 }
+
 
