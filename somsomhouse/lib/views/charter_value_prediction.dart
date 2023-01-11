eafd52f594c92560832a6ed58f077e71f799d0c8
soonhyong <mikekwon42@gmail.com>
2023-01-11 12:20:43 +0900

    [수정] stateful 에서 stateless 로

--- somsomhouse/lib/views/charter_value_prediction.dart
+++ somsomhouse/lib/views/charter_value_prediction.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 
-
-class CharterPrediction extends StatefulWidget {
+class CharterPrediction extends StatelessWidget {
   const CharterPrediction({super.key});
 
-  @override
-  State<CharterPrediction> createState() => _CharterPredictionState();
-}
-
-class _CharterPredictionState extends State<CharterPrediction> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -17,78 +11,81 @@ class _CharterPredictionState extends State<CharterPrediction> {
       },
       child: Scaffold(
         appBar: AppBar(
-        title: const Text('전세값 예측해 보기'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(bottom: 50),
-                child:  Text(
-                  '아파트이름',
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: Colors.transparent,
-                    shadows: [Shadow(offset: Offset(0, -20),
-                    color: Colors.black54)],
-                    decoration: TextDecoration.underline,
-                    decorationStyle: TextDecorationStyle.dashed,
-                    decorationColor: Colors.orange,
-                  ),
-                  ),
-                ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: '임대 면적',
-                  labelText: '임대 면적',
-                  prefixIcon: const Icon(Icons.content_paste),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+          title: const Text('전세값 예측해 보기'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 50),
+                  child: Text(
+                    '아파트이름',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.transparent,
+                      shadows: [
+                        Shadow(offset: Offset(0, -20), color: Colors.black54)
+                      ],
+                      decoration: TextDecoration.underline,
+                      decorationStyle: TextDecorationStyle.dashed,
+                      decorationColor: Colors.orange,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
+                TextField(
                   decoration: InputDecoration(
-                    hintText: '층 수',
-                    labelText: '층 수',
-                    prefixIcon: const Icon(Icons.show_chart),
+                    hintText: '임대 면적',
+                    labelText: '임대 면적',
+                    prefixIcon: const Icon(Icons.content_paste),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10.0),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20,),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: '계약 계절',
-                  labelText: '계약 계절',
-                  prefixIcon: const Icon(Icons.sunny_snowing),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: '층 수',
+                      labelText: '층 수',
+                      prefixIcon: const Icon(Icons.show_chart),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: ElevatedButton( 
-                  onPressed: (){
-                    //
-                  }, child: const Text('시세 예측해 보기')),
-              ),
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: '계약 계절',
+                    labelText: '계약 계절',
+                    prefixIcon: const Icon(Icons.sunny_snowing),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        //
+                      },
+                      child: const Text('시세 예측해 보기')),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      // body: Text(Provider.of<FinalViewModel>(context).apartName,),
+        // body: Text(Provider.of<FinalViewModel>(context).apartName,),
       ),
     );
   }
 }
-
